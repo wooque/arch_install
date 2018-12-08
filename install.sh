d79950e318aa213093d84c0770c455c8b35cc98e
Vuk Mirovic <wooque@gmail.com>
2018-12-08 10:59:27 +0100

    Migrate to maximbaz repo for chromium-vaapi

--- install.sh
+++ install.sh
@@ -92,6 +92,9 @@ cat fstab >> /etc/fstab
 chown -R vuk:wheel /opt/arch_install
 cd /opt/arch_install
 
+echo_sleep "Add maximbaz repo..."
+echo -e "[maximbaz]\nServer = https://pkgbuild.com/~maximbaz/repo/" >> /etc/pacman.conf
+
 echo_sleep "Install yay..."
 sed -i 's/#Color/Color/' /etc/pacman.conf
 wget "https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=yay" -O PKGBUILD
